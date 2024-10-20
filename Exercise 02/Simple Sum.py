57236f9c0478340554ed47b98640f64333239942
Tasnim-Aslam-shaikh <tasnimshaikh2006@gmail.com>
2024-10-20 22:49:42 +0400

    Simple Sum

---
+++ Exercise 02/Simple Sum.py
@@ -0,0 +1,18 @@
+### Steps:
+#1. Declare a variable and initialize it with the integer value '8'.
+#2. Declare a second variable and initialize it with the integer value '10'.
+#3. Declare a third variable that stores the sum of first two numbers.
+#4. Print the value of the sum of the sum to the console
+
+#simple sums
+#inserting an integer to the first variable
+apple = (int(8))
+
+#inserting an integer to the second variable
+banana = (int(10))
+
+#inserting both the first and second variable to the third variable
+chikoo = (apple + banana)
+
+#print the variable charle
+print(chikoo)
